95729d0edb2977d11030b0d7096e78a97b2530e4
Rishav Ambasta <kooolrishav@gmail.com>
2017-07-18 07:33:28 +0530

    Initial commit

---
+++ app/src/main/java/in/co/ecil/hisaab_admin/HisaabActivity.java
@@ -0,0 +1,19 @@
+package in.co.ecil.hisaab_admin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rishav
+ * on 17/7/17.
+ */
+
+public class HisaabActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_bill);
+    }
+}
